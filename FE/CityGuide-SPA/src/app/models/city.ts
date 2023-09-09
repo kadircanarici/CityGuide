5eca5f140b76842bd74f3b7484acf5ded76dcd3e
kadircanarici <kadircanarc@gmail.com>
2023-09-09 19:57:37 +0300

    City için service ve implementasyon işlemi

--- FE/CityGuide-SPA/src/app/models/city.ts
+++ FE/CityGuide-SPA/src/app/models/city.ts
@@ -1,9 +1,10 @@
-import { Photo } from "./photo";
+import { Photo } from './photo';
 
 export class City {
-    id!: number;
-    name!: string;
-    description!: string;
-    userId!: number;
-    photos!: Photo[];
+  id!: number;
+  name!: string;
+  description!: string;
+  userId!: number;
+  photos!: Photo[];
+  photoUrl!: string;
 }
