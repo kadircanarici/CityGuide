1dd5e63903fb9b42929a8460113bb6eff48c3d8b
kadircanarici <kadircanarc@gmail.com>
2023-09-09 18:59:23 +0300

    navbar ve city comp. eklendi.

--- FE/CityGuide-SPA/src/app/app.module.ts
+++ FE/CityGuide-SPA/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      NavComponent,
+      CityComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
